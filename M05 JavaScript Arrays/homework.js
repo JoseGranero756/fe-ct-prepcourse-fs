78c5bf2502f70192b3e9bd2d6842f153767a010e
Jose Granero <josegranero756@gmail.com>
2023-12-06 19:06:31 -0300

    pequeños cambios al practico m5

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -147,7 +147,7 @@ function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
-   primer = array[0];
+   var primer = array[0];
    for (i = 1 ; i < array.length ; i++) {
       if (array[i] != primer) {
          return false;
